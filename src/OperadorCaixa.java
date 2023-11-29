0bc2a18eaa4680ea629c6bc1f95a03976a3b70c2
Neo40 <tron36.cc@gmail.com>
2023-11-29 19:45:28 -0300

    Criação de classe com metodo toString

---
+++ src/OperadorCaixa.java
@@ -0,0 +1,12 @@
+public class OperadorCaixa extends FuncionarioCLT {
+
+    @Override
+    public String toString() {
+        return "OperadorCaixa{" +
+                "nome='" + nome + '\'' +
+                ", documento='" + documento + '\'' +
+                ", valorSalario=" + valorSalario +
+                ", endereco=" + endereco +
+                '}';
+    }
+}
