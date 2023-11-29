8f0d1e28596967f07b8753da935bb163604c8d4b
Neo40 <tron36.cc@gmail.com>
2023-11-29 19:19:41 -0300

    Mudança de endereço vindo do construtor

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 
 public class Main {
     public static void main(String[] args) {
-        Endereco endereco = new Endereco();
+        Endereco endereco = new Endereco("Rua do Funcionario", "Complemento do Funcionario", "bairro do funcionario");
+        String rua = endereco.getRua();
+        System.out.println(rua);
+        endereco.setRua("Rua do Funcionario Fulano");
+        String rua1 = endereco.getRua();
+
+        System.out.println(rua1);
         System.out.println(endereco);
     }
 }
