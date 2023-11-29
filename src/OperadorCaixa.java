7f0555b6dded10b945b1070690b56961c318bdcb
Neo40 <tron36.cc@gmail.com>
2023-11-29 19:50:16 -0300

    Criação de construtor com referencia super

--- src/OperadorCaixa.java
+++ src/OperadorCaixa.java
@@ -1,5 +1,9 @@
 public class OperadorCaixa extends FuncionarioCLT {
 
+    public OperadorCaixa(String nome, String documento, Double valorSalario, Endereco endereco) {
+        super(nome, documento, valorSalario, endereco);
+    }
+
     @Override
     public String toString() {
         return "OperadorCaixa{" +
