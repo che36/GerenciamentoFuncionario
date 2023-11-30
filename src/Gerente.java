866569373ccd1919ad935969ee48e434738a2853
Neo40 <tron36.cc@gmail.com>
2023-11-29 21:11:53 -0300

    Sobrescrita de metodo toString

--- src/Gerente.java
+++ src/Gerente.java
@@ -5,4 +5,19 @@ public class Gerente extends FuncionarioPJ implements CalculaBonificacao {
     public void calculaBonificacao(Double porcentagemBonificacao) {
         this.valorBonificacao = super.getValorRemuneracao() * (porcentagemBonificacao / 100) + 100d;
     }
+
+
+    @Override
+    public String toString() {
+        return "FuncionarioPJ{" +
+                "nome='" + super.getNome() + '\'' +
+                ", documento='" + super.getDocumento() + '\'' +
+                ", valorHora=" + super.getValorHora() +
+                ", endereco=" + super.getEndereco() +
+                ", valorRemuneracao=" + super.getValorRemuneracao() +
+                ", valorBonificacao=" + this.valorBonificacao +
+                ", horasTrabalhadas=" + super.getHorasTrabalhadas() +
+                '}';
+    }
+}
 }
